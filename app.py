abb34c255a5db0dc8d1ea714c42bf0c95a3aef9d
01star01ek <01star01ek@gmail.com>
2024-04-23 23:55:30 +0900

    [feat] main 페이지

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/main')
+def main() :
+   return render_template('main.html')
+
+
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
